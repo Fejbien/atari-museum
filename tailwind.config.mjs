e53c7648bd74ff9eda9e0336c18cacd3aa41bb69
Fabian Sucholas <fabian.sucholas123@gmail.com>
2024-04-03 23:48:58 +0200

    tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        colors: {
+            mainBg: "#32cc33",
+        },
+        extend: {
+            height: {
+                screen: [
+                    "100vh /* fallback for Opera, IE and etc. */",
+                    "100dvh",
+                ],
+            },
+            width: {
+                screen: [
+                    "100vw /* fallback for Opera, IE and etc. */",
+                    "100dvw",
+                ],
+            },
+            fontFamily: {
+                pixeledFont: ['"Pixelify Sans"', "sans-serif"],
+            },
+        },
+        screens: {
+            "2xl": { max: "1535px" },
+            // => @media (max-width: 1535px) { ... }
+
+            xl: { max: "1279px" },
+            // => @media (max-width: 1279px) { ... }
+
+            lg: { max: "1023px" },
+            // => @media (max-width: 1023px) { ... }
+
+            md: { max: "767px" },
+            // => @media (max-width: 767px) { ... }
+
+            sm: { max: "639px" },
+            // => @media (max-width: 639px) { ... }
+        },
+    },
+    plugins: [],
+};
