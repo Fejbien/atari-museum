acad24d0269e89ffecde5075363ba07b0b62c6d0
Fabian Sucholas <fabian.sucholas123@gmail.com>
2024-11-03 21:01:13 +0100

    google button

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -8,9 +8,9 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const password = formData.get("password")?.toString();
     const provider = formData.get("provider")?.toString();
 
-    const validProviders = ["google", "github", "discord"];
+    const validProviders = ["google"];
 
-    console.log(validProviders);
+    //console.log(validProviders);
 
     if (provider && validProviders.includes(provider)) {
         const { data, error } = await supabase.auth.signInWithOAuth({
