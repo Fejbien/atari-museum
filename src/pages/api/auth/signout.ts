52432c8ac3be59455c304034f8bd8c8b28e245cd
Fabian Sucholas <fabian.sucholas123@gmail.com>
2024-11-03 20:51:38 +0100

    auth fix

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -3,5 +3,5 @@ import type { APIRoute } from "astro";
 export const GET: APIRoute = async ({ cookies, redirect }) => {
     cookies.delete("accessToken", { path: "/" });
     cookies.delete("refreshToken", { path: "/" });
-    return redirect("/signin");
+    return redirect("/login");
 };
