2aa330d714be9d7a5555eff76554334fb2a7b603
Fabian Sucholas <fabian.sucholas123@gmail.com>
2024-11-03 11:51:57 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import solidJs from "@astrojs/solid-js";
 import react from "@astrojs/react";
 
 // https://astro.build/config
