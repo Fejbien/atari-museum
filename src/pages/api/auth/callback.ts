7f2f4f0fc795912cc2a6eeca5c60ce43ee69ccb0
Fabian Sucholas <fabian.sucholas123@gmail.com>
2024-11-03 19:32:25 +0100

    test logowania z google

---
+++ src/pages/api/auth/callback.ts
@@ -0,0 +1,38 @@
+import { createServerClient, parseCookieHeader } from "@supabase/ssr";
+import { type APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request, cookies, redirect }) => {
+    const requestUrl = new URL(request.url);
+    const code = requestUrl.searchParams.get("code");
+    const next = requestUrl.searchParams.get("next") || "/";
+
+    if (code) {
+        const supabase = createServerClient(
+            import.meta.env.SUPABASE_URL,
+            import.meta.env.SUPABASE_API_KEY,
+            {
+                cookies: {
+                    getAll() {
+                        return parseCookieHeader(
+                            request.headers.get("Cookie") ?? ""
+                        );
+                    },
+                    setAll(cookiesToSet) {
+                        cookiesToSet.forEach(({ name, value, options }) =>
+                            cookies.set(name, value, options)
+                        );
+                    },
+                },
+            }
+        );
+
+        const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+        if (!error) {
+            return redirect(next);
+        }
+    }
+
+    // return the user to an error page with instructions
+    return redirect("/auth/auth-code-error");
+};
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_API_KEY
+    import.meta.env.SUPABASE_URL,
+    import.meta.env.SUPABASE_API_KEY,
+    {
+        auth: {
+            flowType: "pkce",
+        },
+    }
 );
