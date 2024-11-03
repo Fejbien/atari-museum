1d9ff36580416fed3df3af62d0d9bbd42d4e8215
Fabian Sucholas <fabian.sucholas123@gmail.com>
2024-11-03 12:14:29 +0100

    deno?

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,10 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string
-  readonly SUPABASE_API_KEY: string
+    readonly SUPABASE_URL: string;
+    readonly SUPABASE_API_KEY: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv;
 }
